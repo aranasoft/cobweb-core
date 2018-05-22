66c3e13f24ae69fb9015692605e3357cf944dcdf
Jay Harris <jay@aranasoft.com>
2018-05-22 09:43:02 -0700

    feat(guid): add support for generating a sequential Guid

---
+++ test/Core.Tests/GivenSequentialGuid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Cobweb.Tests {
+    [TestFixture]
+    public class GivenSequentialGuid {
+        [Test]
+        public void ItShouldGenerateUniqueGuids() {
+            const int generatedValueCount = 1000;
+            var values = new HashSet<Guid>();
+            for (var i = 0; i < generatedValueCount; i++) {
+                values.Add(SequentialGuid.NewGuid());
+            }
+
+            // All values should be unique
+            values.Count.Should().Be(generatedValueCount);
+        }
+    }
+}
